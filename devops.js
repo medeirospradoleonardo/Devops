f1cfbbada5dee326975d99271389347399ea625f
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2025-02-04 17:21:47 -0300

    atts

--- devops.js
+++ devops.js
@@ -54,49 +54,12 @@ async function getWorkItem(workItemId, fields){
   return await fetch(request)
 }
 
-async function init() {
-  // const result1 = await (await modifyField(2, 'System.Title', 'Titulo Teste')).json()
-  // const result2 = await (await modifyResolution(2, '<h3>Apex Classes: </h3><ul><li>ServiceContractPDFCtrl</li><li>OrderPDFCtrl</li></ul><h3>Visualforce Pages: </h3><ul><li>ServiceContractPDF</li><li>OrderPDF</li></ul>')).json()
-
-  const workItemId = (args?.id)?.toString().replace(/[^0-9]/g, '')
-  const newResolutionXML = args?.resolution
-
-  const newResolutionJSON = JSON.parse(convert.xml2json(newResolutionXML.substring(0, newResolutionXML.indexOf('</Package>') + 10), {compact: true, spaces: 4}))
-  
-  const newResolutionHTML = formatResolutionJSON(newResolutionJSON)
-  // console.log(newResolutionHTML)
-  const newResolutionHTMLToJSON = JSON.parse(convert.xml2json(newResolutionHTML, {compact: true, spaces: 4}))
-  console.log(convert.xml2json(newResolutionHTML, {compact: true, spaces: 4}))
-  
-  // let result = await (await modifyResolution(workItemId, newRecsolutionHTML)).json()
-  
-  const workItemReceived = await (await getWorkItem(workItemId, ['Microsoft.VSTS.Common.Resolution'])).json()
-  const currentResolution = workItemReceived?.fields['Microsoft.VSTS.Common.Resolution']
-  // // const currentResolutionHTML = '<body>' + currentResolution + '</body>'
-  const currentResolutionHTML = currentResolution.includes('<body>') ? currentResolution : '<body>' + currentResolution + '</body>'
-  
-  // const currentResolutionJSON = JSON.parse(convert.xml2json(currentResolutionHTML, {compact: true, spaces: 4}))
-  
-  console.log(currentResolutionHTML)
-  // console.log(currentResolutionJSON)
-  // // currentResolutionJSON.body.div = [currentResolutionJSON.body.div]
-  // console.log(currentResolutionJSON)
-  // // const mergedResolutionJSON = _.defaultsDeep(currentResolutionJSON, newResolutionHTMLToJSON)
-  // const mergedResolutionJSON = concatResolutions(currentResolutionJSON, newResolutionHTMLToJSON)
-  // console.log(JSON.stringify(mergedResolutionJSON))
-  // const mergedResolutionHTML = convert.json2xml(mergedResolutionJSON, {compact: true, spaces: 4})
-  // console.log(mergedResolutionHTML)
-  
-  // result = await (await modifyResolution(workItemId, mergedResolutionHTML)).json()
-  // console.log(result)
-}
-
 function formatResolutionJSON(resolutionJSON) {
-  return '<div automatedGenerated=true>' + resolutionJSON?.Package?.types?.reduce(
+  return '<body><div class=\"automaticallyGenerated\">' + resolutionJSON?.Package?.types?.reduce(
     (accumulatorType, currentType) => accumulatorType + '<div>' + (createHeading(currentType?.name?._text) + currentType?.members?.reduce(
       (accumulatorMember, currentMember, index) => accumulatorMember + (createItem(currentMember?._text, index == currentType?.members?.length - 1)), '<ul>'
     )) + '</ul></div>', ''
-  ) + '</div>'
+  ) + '</div></body>'
 }
 
 function createHeading(headingName){
@@ -108,13 +71,12 @@ function createItem(itemName, isLast){
 }
 
 function concatResolutions(currentResolution, newResolution) {
-  const isArray = !!currentResolution?.body?.div?.length
-  let automatedGeneratedDiv = isArray ? currentResolution?.body?.div?.find((div) => !!div._attributes?.automatedGenerated) : currentResolution?.body?.div?._attributes?.automatedGenerated
-
+  let automatedGeneratedDiv = currentResolution?.body?.div?.find((div) => div?._attributes?.class == 'automaticallyGenerated')
+  
   if(automatedGeneratedDiv){
     automatedGeneratedDiv = {...newResolution}
   }else{
-    currentResolution.body.div = currentResolution?.body?.div ? [currentResolution?.body?.div].concat(newResolution?.div) : newResolution?.div
+    currentResolution.body.div = currentResolution?.body?.div.concat(newResolution?.body?.div)
   }
 
   return currentResolution
@@ -142,29 +104,31 @@ function mergeDeep(target, ...sources) {
   return mergeDeep(target, ...sources);
 }
 
-const xml = `
-<?xml version="1.0" encoding="UTF-8"?>
-<Package xmlns="http://soap.sforce.com/2006/04/metadata">
-    <types>
-        <members>InformationReportTH</members>
-        <members>InformationReportTHTest</members>
-        <members>RepresentanteLegalFixtureFactory</members>
-        <members>RepresentanteLegalTH</members>
-        <members>RepresentanteLegalTHTest</members>
-        <name>ApexClass</name>
-    </types>
-    <types>
-        <members>InformationReport</members>
-        <members>RepresentanteLegal</members>
-        <name>ApexTrigger</name>
-    </types>
-    <version>56.0</version>
-</Package><?xml version="1.0" encoding="UTF-8"?>
-<Package xmlns="http://soap.sforce.com/2006/04/metadata">
-    <version>56.0</version>
-</Package>
+async function init() {
+  const workItemId = (args?.id)?.toString().replace(/[^0-9]/g, '')
+  const newResolutionXML = args?.resolution
 
-`
+  const newResolutionJSON = JSON.parse(convert.xml2json(newResolutionXML.substring(0, newResolutionXML.indexOf('</Package>') + 10), {compact: true, spaces: 4}))
+  const newResolutionHTML = formatResolutionJSON(newResolutionJSON)
+  
+  // colocando a nova direto
+  // let result = await (await modifyResolution(workItemId, newResolutionHTML)).json()
+  
+  const workItemReceived = await (await getWorkItem(workItemId, ['Microsoft.VSTS.Common.Resolution'])).json()
+  const currentResolution = workItemReceived?.fields['Microsoft.VSTS.Common.Resolution']
+  const currentResolutionHTML = (currentResolution.includes('<body>') ? currentResolution : '<body>' + currentResolution + '</body>').replace('automaticallyGenerated', '\"automaticallyGenerated\"')
+  
+  const currentResolutionJSON = JSON.parse(convert.xml2json(currentResolutionHTML, {compact: true, spaces: 4}))
+  
+  currentResolutionJSON.body.div = currentResolutionJSON.body?.div?.length ? currentResolutionJSON.body.div : [currentResolutionJSON.body.div]
+  const newResolutionHTMLToJSON = JSON.parse(convert.xml2json(newResolutionHTML, {compact: true, spaces: 4}))
+  
+  // const mergedResolutionJSON = _.defaultsDeep(currentResolutionJSON, newResolutionHTMLToJSON)
+  const mergedResolutionJSON = concatResolutions(currentResolutionJSON, newResolutionHTMLToJSON)
+  const mergedResolutionHTML = convert.json2xml(mergedResolutionJSON, {compact: true, spaces: 4})
+  
+  result = await (await modifyResolution(workItemId, mergedResolutionHTML)).json()
+}
 
 init()
 
