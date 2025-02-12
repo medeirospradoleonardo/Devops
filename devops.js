0db9fa9dfafd254ebb0d98f6e51c4670a218780a
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2025-02-12 13:24:54 -0300

    atts

--- devops.js
+++ devops.js
@@ -1,24 +1,40 @@
-const BASE_URL = 'dev.azure.com'
-const ORGANIZATION = 'l30'
-const PROJECT = 'l30'
-const API_VERSION = '7.2-preview'
-const LOGIN = 'basic'
-const PASSWORD = '54zaKAiLRTnWqmWqNok1PmxP5QZcdduVnf3rdDNKzTuasjbQe3yXJQQJ99BAACAAAAAAAAAAAAASAZDOcztX'
-const URL = `https://${BASE_URL}/${ORGANIZATION}/${PROJECT}/_apis/wit/workitems/`
-
-const AUTOMATED_LABEL = 'Gerado Automaticamente'
-
 const args = require('minimist')(process.argv.slice(2))
 const convert = require('xml-js');
 const fs = require('node:fs');
+require('dotenv').config()
+
+const BASE_URL = process.env.DEV_OPS_BASE_URL
+const ORGANIZATION = process.env.DEV_OPS_ORGANIZATION
+const PROJECTS = process.env.DEV_OPS_PROJECTS?.split(';').map((project) => project.trim())
+const API_VERSION = process.env.DEV_OPS_API_VERSION
+const LOGIN = process.env.DEV_OPS_LOGIN
+const PASSWORD = process.env.DEV_OPS_PASSWORD
+const URL = `https://${BASE_URL}/${ORGANIZATION}/{project}/_apis/wit/workitems/`
+
+const AUTOMATED_LABEL = '=========== Gerado Automaticamente ==========='
 
 async function modifyResolution(workItemId, resolution) {
   return await modifyField(workItemId, 'Microsoft.VSTS.Common.Resolution', resolution)
 }
 
+async function getProjectName(workItemId) {
+  let project
+  for (const projectName of PROJECTS) {
+    const workItem = await getWorkItem(workItemId, projectName)
+
+    if (workItem) {
+      project = projectName
+      break;
+    }
+  }
+
+  return project
+}
+
 async function modifyField(workItemId, field, value) {
 
-  const url = URL + `${workItemId}?api-version=${API_VERSION}`
+  const projectName = await getProjectName(workItemId)
+  const url = URL.replace('{project}', projectName) + `${workItemId}?api-version=${API_VERSION}`
 
   let headers = new Headers()
   headers.set('Authorization', 'Basic ' + btoa(`${LOGIN}:${PASSWORD}`))
@@ -41,8 +57,8 @@ async function modifyField(workItemId, field, value) {
   return await fetch(request)
 }
 
-async function getWorkItem(workItemId, fields){
-  const url = URL + `${workItemId}?fields=${fields.join(',')}&api-version=${API_VERSION}`
+async function getWorkItem(workItemId, projectName, fields) {
+  const url = URL.replace('{project}', projectName) + `${workItemId}?fields=${fields?.join(',')}&api-version=${API_VERSION}`
 
   let headers = new Headers()
   headers.set('Authorization', 'Basic ' + btoa(`${LOGIN}:${PASSWORD}`))
@@ -60,24 +76,24 @@ function formatResolutionJSON(resolutionJSON) {
   return '<body><div><h2>' + AUTOMATED_LABEL + '</h2>' + resolutionJSON?.Package?.types?.reduce(
     (accumulatorType, currentType) => accumulatorType + '<div>' + (createHeading(currentType?.name?._text) + (currentType?.members?.length ? currentType?.members?.reduce(
       (accumulatorMember, currentMember, index) => accumulatorMember + (createItem(currentMember?._text, index == currentType?.members?.length - 1)), '<ul>'
-    ) : '<ul>' + createItem(currentType?.members?._text, true)))  + '</ul></div>', ''
+    ) : '<ul>' + createItem(currentType?.members?._text, true))) + '</ul></div>', ''
   ) + '</div></body>'
 }
 
-function createHeading(headingName){
+function createHeading(headingName) {
   return `<h3>${headingName.trim()}: </h3>`
 }
 
-function createItem(itemName, isLast){
+function createItem(itemName, isLast) {
   return `<li>${itemName.trim()}${isLast ? '. ' : '; '}</li>`
 }
 
 function concatResolutions(currentResolution, newResolution) {
   let automatedGeneratedDivIndex = currentResolution?.body?.div?.findIndex((div) => div?.h2?.length ? div?.h2?.find((h2) => h2._text?.trim() == AUTOMATED_LABEL) : div?.h2?._text?.trim() == AUTOMATED_LABEL)
-  
-  if(automatedGeneratedDivIndex >= 0){
+
+  if (automatedGeneratedDivIndex >= 0) {
     currentResolution.body.div[automatedGeneratedDivIndex].div = newResolution.body.div.div
-  }else{
+  } else {
     currentResolution.body.div = currentResolution?.body?.div ? currentResolution?.body?.div.concat(newResolution?.body?.div) : [newResolution?.body?.div]
   }
 
@@ -93,35 +109,35 @@ async function runFile(fileName, workItemId, concat) {
     console.error(err)
   }
 
-  runResolution(newResolutionXML, workItemId, concat)  
+  runResolution(newResolutionXML, workItemId, concat)
 }
 
 async function runResolution(resolution, workItemId, concat = true) {
 
-  const newResolutionJSON = JSON.parse(convert.xml2json(resolution, {compact: true, spaces: 4}))
+  const newResolutionJSON = JSON.parse(convert.xml2json(resolution, { compact: true, spaces: 4 }))
   const newResolutionHTML = formatResolutionJSON(newResolutionJSON)
-  
+
   //colocando a nova direto
-  if(!concat){
+  if (!concat) {
     return await (await modifyResolution(workItemId, newResolutionHTML)).json()
-  } 
-  
+  }
+
   //Dando upsert
-  const workItemReceived = await (await getWorkItem(workItemId, ['Microsoft.VSTS.Common.Resolution'])).json()
+  const projectName = await getProjectName(workItemId)
+  const workItemReceived = await (await getWorkItem(workItemId, projectName, ['Microsoft.VSTS.Common.Resolution'])).json()
   let currentResolution = workItemReceived?.fields['Microsoft.VSTS.Common.Resolution']
   currentResolution = currentResolution.replace(/<([A-z]+)([^>^/]*)>\s*<\/\1>/gim, '').replaceAll('<br>', '')
-  
-  const currentResolutionHTML = (currentResolution.includes('<body>') ? currentResolution : '<body>' + currentResolution + '</body>')
-  console.log(currentResolutionHTML)
 
-  const currentResolutionJSON = JSON.parse(convert.xml2json(currentResolutionHTML, {compact: true, spaces: 4}))
-  
+  const currentResolutionHTML = (currentResolution.includes('<body>') ? currentResolution : '<body>' + currentResolution + '</body>')
+
+  const currentResolutionJSON = JSON.parse(convert.xml2json(currentResolutionHTML, { compact: true, spaces: 4 }))
+
   currentResolutionJSON.body.div = currentResolutionJSON.body.div ? (currentResolutionJSON.body?.div?.length ? currentResolutionJSON.body.div : [currentResolutionJSON.body.div]) : []
-  const newResolutionHTMLToJSON = JSON.parse(convert.xml2json(newResolutionHTML, {compact: true, spaces: 4}))
-  
+  const newResolutionHTMLToJSON = JSON.parse(convert.xml2json(newResolutionHTML, { compact: true, spaces: 4 }))
+
   const mergedResolutionJSON = concatResolutions(currentResolutionJSON, newResolutionHTMLToJSON)
-  const mergedResolutionHTML = convert.json2xml(mergedResolutionJSON, {compact: true, spaces: 4})
-  
+  const mergedResolutionHTML = convert.json2xml(mergedResolutionJSON, { compact: true, spaces: 4 })
+
   return await (await modifyResolution(workItemId, mergedResolutionHTML)).json()
 }
 
@@ -132,7 +148,7 @@ async function init() {
 
   fileName && runFile(fileName, workItemId, false)
   resolution && runResolution(resolution, workItemId)
-  
+
 }
 
 init()
